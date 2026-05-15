51cc4df3d79baa8ed59ab800bcc921ecef6959f0
Joao <bielr7dantas@gmail.comjodantasz/exercicios_python>
2026-05-15 07:54:34 -0300

    first commit

---
+++ Exercicios_python.py
@@ -0,0 +1,127 @@
+
+print ("=======================================")
+#EXERCÍCIO 1
+print ("Exercício 1")
+
+#Um aluno tem 10 anos. Armazene essa idade em uma variável e exiba seu tipo
+idade = 10
+print ("idade:", idade)
+
+#Exiba seu tipo
+print ("Tipo:", type(idade))
+
+print ("=======================================")
+
+#EXERCÍCIO 2
+print ("Exercício 2")
+
+# A temperatura média é 23.5°C. Armazene e mostre seu tipo.
+temperatura = 23.5
+print ("Temperatura Atual", temperatura)
+
+# Armazene esse valor e mostre seu tipo
+print ("Tipo:", type(temperatura))
+
+# Divisor
+print ("=======================================")
+
+# Exercício 3
+print ("Exercício 3")
+
+#Crie um número complexo representando uma impedância elétrica de 5 + 8j e mostre sua parte real.
+impedancia = 5 + 8j
+print (impedancia.real)
+
+print ("impedancia / Parte real: ", impedancia.real)
+
+# Divisor
+print ("=======================================")
+
+# Exercício 4
+print ("Exercício 4")
+
+# # Mostre a parte imaginária do número complexo criado no exercício anterior.
+print ("impedancia / Parte Imaginária: ", impedancia.imag)
+
+# Divisor
+print ("=======================================")
+
+# Exercício 5
+print ("Exercício 5")
+
+população = 8_000_000_000
+# Mostrando a população 
+print (população)
+
+# Mostrando tipo
+print ("Tipo:", type(população))
+
+# Divisor
+print ("=======================================")
+
+# Exercício 6
+print ("Exercício 6")
+
+# Verificando se o número 7 é do tipo inteiro
+print (type(7))
+
+# Tipo do número 7
+print ("Tipo do número 7:", type(7))
+
+# Divisor
+print ("=======================================")
+
+# Exercício 7
+print ("Exercício 7")
+
+# Criação Variável
+aprovado = True
+
+# Mostrando o valor da variável
+print (aprovado)
+
+# Tipo da variável
+print ("Tipo:", type(aprovado))
+
+# Divisor
+print ("=======================================")
+
+# Exercício 8
+print ("Exercício 8")   
+
+# Criação Variável
+resultado = True + False
+
+# Mostrando o valor da variável
+print (resultado)
+
+# Tipo da variável
+print ("Tipo:", type(resultado))
+
+# Divisor
+print ("=======================================")
+
+# Exercício 9
+print ("Exercício 9")
+
+# Criação Variável
+Maior_número = 1.8e307
+
+# Mostrando o valor da variável
+print (Maior_número)
+
+# Tipo da variável
+print ("Tipo:", type(Maior_número))
+
+# Divisor
+print ("=======================================")
+
+# Exercício 10
+print ("Exercício 10")  
+
+# Criação Variável
+numero = 10
+print (bin(numero)[2:])
+
+# Divisor
+print ("=======================================")
