cb7bf88cdfc5d401897588a77056b6fb5ec3aff5
João Dantas <bielr7dantas@gmail.com>
2026-06-10 15:06:56 -0300

    Add files via upload

---
+++ salario.py
@@ -0,0 +1,22 @@
+# EXERCÍCIO — CÁLCULO DE SALÁRIO COM IMPOSTO
+
+# Parte 1
+salario = 2000
+imposto = 10
+
+valor_imposto = salario * imposto / 100
+salario_final = salario - valor_imposto
+
+print("Salário:", salario)
+print("Imposto:", valor_imposto)
+print("Salário final:", salario_final)
+
+# Parte 2
+salario = float(input("Digite seu salário: "))
+imposto = float(input("Digite a taxa de imposto: "))
+
+valor_imposto = salario * imposto / 100
+salario_final = salario - valor_imposto
+
+print("Imposto:", valor_imposto)
+print("Salário final:", salario_final)
